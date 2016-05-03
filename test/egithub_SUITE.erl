35eda08377833dc47ff16b0b9e8ba2df6ce33b3a
Euen <euenlopez@gmail.com>
2016-05-03 17:51:45 -0300

    [#103] fix hackney issue in suite

--- test/egithub_SUITE.erl
+++ test/egithub_SUITE.erl
@@ -64,21 +64,21 @@ pull_reqs(_Config) ->
     PRFilesFun = match_fun("/repos/user/repo/pulls/1/files", get),
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
-    meck:expect(hackney, send_request, PRFilesFun),
+    meck:expect(hackney, request, PRFilesFun),
     {ok, _} = egithub:pull_req_files(Credentials, "user/repo", 1),
 
     PRCommentLineFun = match_fun("/repos/user/repo/pulls/1/comments", post),
-    meck:expect(hackney, send_request, PRCommentLineFun),
+    meck:expect(hackney, request, PRCommentLineFun),
     {ok, _} = egithub:pull_req_comment_line(Credentials, "user/repo", 1,
                                             "SHA", <<"file-path">>,
                                             5, "comment text"),
 
     Self = self(),
-    PRCommentLineQueueFun = fun(_, {_, _, _, _}) ->
+    PRCommentLineQueueFun = fun(_, _, _, _) ->
                                 Self ! ok,
                                 {ok, 200, [], #client{}}
                             end,
-    meck:expect(hackney, send_request, PRCommentLineQueueFun),
+    meck:expect(hackney, request, PRCommentLineQueueFun),
     ok = egithub:pull_req_comment_line(Credentials, "user/repo", 1,
                                        "SHA", <<"file-path">>,
                                        5, "comment text",
@@ -87,7 +87,7 @@ pull_reqs(_Config) ->
 
     PRCommentsFun = match_fun("/repos/user/repo/pulls/1/comments",
                               get),
-    meck:expect(hackney, send_request, PRCommentsFun),
+    meck:expect(hackney, request, PRCommentsFun),
     {ok, _} = egithub:pull_req_comments(Credentials, "user/repo", 1)
   after
     meck:unload(hackney)
@@ -99,26 +99,26 @@ issue_comments(_Config) ->
   meck:new(hackney, [passthrough]),
   try
     IssueCommentFun = match_fun("/repos/user/repo/issues/1/comments", post),
-    meck:expect(hackney, send_request, IssueCommentFun),
+    meck:expect(hackney, request, IssueCommentFun),
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     {ok, _} = egithub:issue_comment(Credentials, "user/repo", 1, "txt"),
 
     Self = self(),
-    IssueCommentQueueFun = fun(_, {post, Url, _, _}) ->
-                               <<"/repos/user/repo/issues/1/comments">> =
-                                 iolist_to_binary(Url),
+    IssueCommentQueueFun = fun(post, Url, _, _) ->
+                               <<"https://api.github.com/"
+                                 "repos/user/repo/issues/1/comments">> = Url,
                                Self ! ok,
                                {ok, 200, [], #client{}}
                            end,
-    meck:expect(hackney, send_request, IssueCommentQueueFun),
+    meck:expect(hackney, request, IssueCommentQueueFun),
     ok = egithub:issue_comment(Credentials, "user/repo", 1,
                                "txt", #{post_method => queue}),
     ok = receive ok -> ok after 5000 -> timeout end,
 
     IssueCommentsFun = match_fun("/repos/user/repo/issues/1/comments",
                                  get),
-    meck:expect(hackney, send_request, IssueCommentsFun),
+    meck:expect(hackney, request, IssueCommentsFun),
     {ok, _} = egithub:issue_comments(Credentials, "user/repo", 1)
   after
     meck:unload(hackney)
@@ -132,30 +132,30 @@ issues(_Config) ->
       BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
       meck:expect(hackney, body, BodyReturnFun),
       CreateIssueFun = match_fun("/repos/user/repo/issues", post),
-      meck:expect(hackney, send_request, CreateIssueFun),
+      meck:expect(hackney, request, CreateIssueFun),
       {ok, _} = egithub:create_issue(Credentials, "user", "repo", "title",
                                      "text", "user", ["bug"]),
 
       AllIssuesFun = match_fun("/issues", get),
-      meck:expect(hackney, send_request, AllIssuesFun),
+      meck:expect(hackney, request, AllIssuesFun),
       {ok, _} = egithub:all_issues(Credentials, #{}),
 
       AllRepoIssuesFun = match_fun("/repos/user/repo/issues", get),
-      meck:expect(hackney, send_request, AllRepoIssuesFun),
+      meck:expect(hackney, request, AllRepoIssuesFun),
       {ok, _} = egithub:all_issues(Credentials, "user/repo", #{}),
 
       IssueUrl = "/issues?direction=asc&filter=assigned&"
                  "sort=created&state=open",
       AllIssuesOpenFun = match_fun(IssueUrl, get),
-      meck:expect(hackney, send_request, AllIssuesOpenFun),
+      meck:expect(hackney, request, AllIssuesOpenFun),
       {ok, _} = egithub:all_issues(Credentials, #{state => "open"}),
 
       UserIssuesFun = match_fun("/user/issues", get),
-      meck:expect(hackney, send_request, UserIssuesFun),
+      meck:expect(hackney, request, UserIssuesFun),
       {ok, _} = egithub:issues_user(Credentials, #{}),
 
       OrgIssuesFun = match_fun("/orgs/foo/issues", get),
-      meck:expect(hackney, send_request, OrgIssuesFun),
+      meck:expect(hackney, request, OrgIssuesFun),
       {ok, _} = egithub:issues_org(Credentials, "foo", #{})
 
   after
@@ -168,7 +168,7 @@ files(_Config) ->
   meck:new(hackney, [passthrough]),
   try
     FileContentFun = match_fun("/repos/user/repo/contents/file?ref=SHA", get),
-    meck:expect(hackney, send_request, FileContentFun),
+    meck:expect(hackney, request, FileContentFun),
     BodyReturnFun = fun(_) -> {ok, <<"{\"content\" : \"\"}">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     {ok, _} = egithub:file_content(Credentials, "user/repo", "SHA", "file")
@@ -184,15 +184,15 @@ users(_Config) ->
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     UserFun = match_fun("/user", get),
-    meck:expect(hackney, send_request, UserFun),
+    meck:expect(hackney, request, UserFun),
     {ok, _} = egithub:user(Credentials),
 
     GadgetCIFun = match_fun("/users/gadgetci", get),
-    meck:expect(hackney, send_request, GadgetCIFun),
+    meck:expect(hackney, request, GadgetCIFun),
     {ok, _} = egithub:user(Credentials, "gadgetci"),
 
     EmailsFun = match_fun("/user/emails", get),
-    meck:expect(hackney, send_request, EmailsFun),
+    meck:expect(hackney, request, EmailsFun),
     {ok, _} = egithub:user_emails(Credentials)
   after
     meck:unload(hackney)
@@ -206,16 +206,16 @@ orgs(_Config) ->
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     OrgsFun = match_fun("/user/orgs", get),
-    meck:expect(hackney, send_request, OrgsFun),
+    meck:expect(hackney, request, OrgsFun),
     {ok, _} = egithub:orgs(Credentials),
 
     OrgsUserFun = match_fun("/users/gadgetci/orgs", get),
-    meck:expect(hackney, send_request, OrgsUserFun),
+    meck:expect(hackney, request, OrgsUserFun),
     {ok, _} = egithub:orgs(Credentials, "gadgetci"),
 
     OrgMembershipFun = match_fun("/user/memberships/orgs/some-organization",
                                  get),
-    meck:expect(hackney, send_request, OrgMembershipFun),
+    meck:expect(hackney, request, OrgMembershipFun),
     {ok, _} = egithub:org_membership(Credentials, "some-organization")
   after
     meck:unload(hackney)
@@ -227,7 +227,7 @@ repos(_Config) ->
   meck:new(hackney, [passthrough]),
   try
     RepoFun = match_fun("/repos/inaka/whatever", get),
-    meck:expect(hackney, send_request, RepoFun),
+    meck:expect(hackney, request, RepoFun),
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     {ok, _} = egithub:repo(Credentials, "inaka/whatever"),
@@ -235,80 +235,84 @@ repos(_Config) ->
     ReposFun = match_fun("/user/repos?"
                          "type=all&sort=full_name&direction=asc&page=1",
                          get),
-    meck:expect(hackney, send_request, ReposFun),
+    meck:expect(hackney, request, ReposFun),
     {ok, _} = egithub:repos(Credentials, #{}),
 
     ReposUserFun = match_fun("/users/gadgetci/repos?page=1",
                              get),
-    meck:expect(hackney, send_request, ReposUserFun),
+    meck:expect(hackney, request, ReposUserFun),
     {ok, _} = egithub:repos(Credentials, "gadgetci", #{}),
 
     AllReposFun = match_fun("/user/repos?"
                              "type=all&sort=full_name&direction=asc&page=1",
                              get),
-    meck:expect(hackney, send_request, AllReposFun),
+    meck:expect(hackney, request, AllReposFun),
     {ok, _} = egithub:all_repos(Credentials, #{}),
     BodyReturn1Fun = fun(_) -> {ok, <<"[1]">>} end,
     BodyReturnEmptyFun = fun(_) -> {ok, <<"[]">>} end,
     AllReposUserFun =
-      fun(_, {get, Url, _, _}) ->
-          case iolist_to_binary(Url) of
-            <<"/users/gadgetci/repos?page=1">> ->
+      fun(get, Url, _, _) ->
+          case Url of
+            <<"https://api.github.com/users/gadgetci/repos?page=1">> ->
               meck:expect(hackney, body, BodyReturn1Fun),
               {ok, 200, [], #client{}};
-            <<"/users/gadgetci/repos?page=2">> ->
+            <<"https://api.github.com/users/gadgetci/repos?page=2">> ->
               meck:expect(hackney, body, BodyReturnEmptyFun),
               {ok, 200, [], #client{}}
           end
       end,
-    meck:expect(hackney, send_request, AllReposUserFun),
+    meck:expect(hackney, request, AllReposUserFun),
     {ok, _} = egithub:all_repos(Credentials, "gadgetci", #{}),
 
     AllReposErrorFun =
-      fun(_, {get, Url, _, _}) ->
+      fun(get, Url, _, _) ->
           case Url of
-            <<"/users/gadgetci/repos?page=1">> ->
+            <<"https://api.github.com/users/gadgetci/repos?page=1">> ->
               meck:expect(hackney, body, BodyReturn1Fun),
               {ok, 200, [], #client{}};
-            <<"/users/gadgetci/repos?page=2">> ->
+            <<"https://api.github.com/users/gadgetci/repos?page=2">> ->
               meck:expect(hackney, body, BodyReturnEmptyFun),
               {ok, 400, [], #client{}}
           end
       end,
-    meck:expect(hackney, send_request, AllReposErrorFun),
+    meck:expect(hackney, request, AllReposErrorFun),
     {error, _} = egithub:all_repos(Credentials, "gadgetci", #{}),
 
     OrgReposFun = match_fun("/orgs/some-org/repos?page=1&per_page=100", get),
-    meck:expect(hackney, send_request, OrgReposFun),
+    meck:expect(hackney, request, OrgReposFun),
     meck:expect(hackney, body, BodyReturnEmptyFun),
     {ok, _} = egithub:org_repos(Credentials, "some-org", #{}),
 
     AllOrgReposFun =
-      fun(_, {get, Url, _, _}) ->
+      fun(get, Url, _, _) ->
           case Url of
-            <<"/orgs/some-org/repos?page=1&per_page=100">> ->
+            <<"https://api.github.com"
+              "/orgs/some-org/repos?page=1&per_page=100">> ->
               meck:expect(hackney, body, BodyReturn1Fun),
               {ok, 200, [], #client{}};
-            <<"/orgs/some-org/repos?page=2&per_page=100">> ->
+            <<"https://api.github.com"
+              "/orgs/some-org/repos?page=2&per_page=100">> ->
               meck:expect(hackney, body, BodyReturnEmptyFun),
               {ok, 200, [], #client{}}
           end
       end,
-    meck:expect(hackney, send_request, AllOrgReposFun),
+    meck:expect(hackney, request, AllOrgReposFun),
     {ok, _} = egithub:all_org_repos(Credentials, "some-org", #{}),
 
     AllOrgReposErrorFun =
-      fun(_, {get, Url, _, _})  ->
+      fun(get, Url, _, _)  ->
           case Url of
-            <<"/orgs/some-org/repos?page=1&per_page=100">> ->
+            <<"https://api.github.com"
+              "/orgs/some-org/repos?page=1&per_page=100">> ->
               meck:expect(hackney, body, BodyReturn1Fun),
               {ok, 200, [], #client{}};
-            <<"/orgs/some-org/repos?page=2&per_page=100">> ->
+            <<"https://api.github.com"
+              "/orgs/some-org/repos?page=2&per_page=100">> ->
               meck:expect(hackney, body, BodyReturnEmptyFun),
               {ok, 400, [], #client{}}
           end
       end,
-    meck:expect(hackney, send_request, AllOrgReposErrorFun),
+    meck:expect(hackney, request, AllOrgReposErrorFun),
     {error, _} = egithub:all_org_repos(Credentials, "some-org", #{})
   after
     meck:unload(hackney)
@@ -322,30 +326,30 @@ teams(_Config) ->
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     TeamsFun = match_fun("/orgs/some-org/teams", get),
-    meck:expect(hackney, send_request, TeamsFun),
+    meck:expect(hackney, request, TeamsFun),
     {ok, _} = egithub:teams(Credentials, "some-org"),
 
     CreateTeamFun = match_fun("/orgs/some-org/teams", post),
-    meck:expect(hackney, send_request, CreateTeamFun),
+    meck:expect(hackney, request, CreateTeamFun),
     {ok, _} = egithub:create_team(Credentials, "some-org", "Team", "", []),
 
     AddTeamRepoFun = match_fun("/teams/1/repos/user/repo",
                                put),
-    meck:expect(hackney, send_request, AddTeamRepoFun),
+    meck:expect(hackney, request, AddTeamRepoFun),
     ok = egithub:add_team_repository(Credentials, 1, "user/repo"),
 
     AddTeamMemberFun = match_fun("/teams/1/members/gadgetci",
                                  put),
-    meck:expect(hackney, send_request, AddTeamMemberFun),
+    meck:expect(hackney, request, AddTeamMemberFun),
     ok = egithub:add_team_member(Credentials, 1, "gadgetci"),
 
     DeleteTeamMemberFun = match_fun("/teams/1/members/gadgetci",
                                     delete),
-    meck:expect(hackney, send_request, DeleteTeamMemberFun),
+    meck:expect(hackney, request, DeleteTeamMemberFun),
     ok = egithub:delete_team_member(Credentials, 1, "gadgetci"),
 
     TeamMembershipFun = match_fun("/teams/1/memberships/gadgetci", get),
-    meck:expect(hackney, send_request, TeamMembershipFun),
+    meck:expect(hackney, request, TeamMembershipFun),
     TeamMembershipBodyFun = fun(_) -> {ok, <<"{\"state\" : \"pending\"}">>} end,
     meck:expect(hackney, body, TeamMembershipBodyFun),
     pending = egithub:team_membership(Credentials, 1, "gadgetci")
@@ -361,18 +365,18 @@ hooks(_Config) ->
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     HooksFun = match_fun("/repos/some-repo/hooks", get),
-    meck:expect(hackney, send_request, HooksFun),
+    meck:expect(hackney, request, HooksFun),
     {ok, _} = egithub:hooks(Credentials, "some-repo"),
 
     CreateHookFun = match_fun("/repos/some-repo/hooks",
                               post),
-    meck:expect(hackney, send_request, CreateHookFun),
+    meck:expect(hackney, request, CreateHookFun),
     {ok, _} = egithub:create_webhook(Credentials, "some-repo",
                                      "url", ["pull_request"]),
 
     DeleteHookFun = match_fun("/repos/some-repo/hooks/url",
                               delete),
-    meck:expect(hackney, send_request, DeleteHookFun),
+    meck:expect(hackney, request, DeleteHookFun),
     ok = egithub:delete_webhook(Credentials, "some-repo", "url")
   after
     meck:unload(hackney)
@@ -386,17 +390,17 @@ collaborators(_Config) ->
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
     CollaboratorsFun = match_fun("/repos/some-repo/collaborators", get),
-    meck:expect(hackney, send_request, CollaboratorsFun),
+    meck:expect(hackney, request, CollaboratorsFun),
     {ok, _} = egithub:collaborators(Credentials, "some-repo"),
 
     AddCollabFun = match_fun("/repos/some-repo/collaborators/username",
                              put),
-    meck:expect(hackney, send_request, AddCollabFun),
+    meck:expect(hackney, request, AddCollabFun),
     ok = egithub:add_collaborator(Credentials, "some-repo", "username"),
 
     DeleteCollabFun = match_fun("/repos/some-repo/collaborators/username",
                                 delete),
-    meck:expect(hackney, send_request, DeleteCollabFun),
+    meck:expect(hackney, request, DeleteCollabFun),
     ok = egithub:remove_collaborator(Credentials, "some-repo", "username")
   after
     meck:unload(hackney)
@@ -410,24 +414,24 @@ statuses(_Config) ->
     StatusesFun = match_fun("/repos/some-repo/commits/ref/statuses", get),
     BodyReturnFun = fun(_) -> {ok, <<"[]">>} end,
     meck:expect(hackney, body, BodyReturnFun),
-    meck:expect(hackney, send_request, StatusesFun),
+    meck:expect(hackney, request, StatusesFun),
     {ok, _} = egithub:statuses(Credentials, "some-repo", "ref"),
 
     CreateStatusFun = match_fun("/repos/some-repo/statuses/SHA",
                                 post),
-    meck:expect(hackney, send_request, CreateStatusFun),
+    meck:expect(hackney, request, CreateStatusFun),
     {ok, _} = egithub:create_status(Credentials, "some-repo", "SHA", pending,
                                     "description", "context"),
 
     CreateStatusUrlFun = match_fun("/repos/some-repo/statuses/SHA",
                                    post),
-    meck:expect(hackney, send_request, CreateStatusUrlFun),
+    meck:expect(hackney, request, CreateStatusUrlFun),
     {ok, _} = egithub:create_status(Credentials, "some-repo", "SHA", pending,
                                     "description", "context", "url"),
 
     CombinedStatusFun = match_fun("/repos/some-repo/commits/ref/status",
                                   get),
-    meck:expect(hackney, send_request, CombinedStatusFun),
+    meck:expect(hackney, request, CombinedStatusFun),
     {ok, _} = egithub:combined_status(Credentials, "some-repo", "ref")
   after
     meck:unload(hackney)
@@ -443,8 +447,9 @@ github_credentials() ->
   egithub:basic_auth("username", "password").
 
 match_fun(Url, Method) ->
-  fun(_, {MethodParam, UrlParam, _, _}) ->
-      Url = binary_to_list(iolist_to_binary(UrlParam)),
+  UrlBin = list_to_binary("https://api.github.com" ++ Url),
+  fun(MethodParam, UrlParam, _, _) ->
+      UrlBin = UrlParam,
       Method = MethodParam,
       RespHeaders = [],
       ClientRef = #client{},
